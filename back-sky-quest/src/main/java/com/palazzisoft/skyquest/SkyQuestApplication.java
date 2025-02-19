d563b68dd05337800b1e9500e85bd4d6a4e7b7bb
Tano Palazzi <pablo.palazzi@xselltechnologies.com>
2025-02-19 15:55:02 -0300

    SQ-2: [BE] Create started project

---
+++ back-sky-quest/src/main/java/com/palazzisoft/skyquest/SkyQuestApplication.java
@@ -0,0 +1,13 @@
+package com.palazzisoft.skyquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyQuestApplication.class, args);
+	}
+
+}
