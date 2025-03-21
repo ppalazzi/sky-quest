59df3d34ed8791f8d882c360bb31b9ba800bee4a
Pablo Palazzi <ppalazzi@gmail.com>
2025-03-21 10:27:22 -0300

    SQ-12 : [BE] Convert sky-quest backend to microservice

--- back-sky-quest/src/main/java/com/palazzisoft/skyquest/SkyQuestApplication.java
+++ back-sky-quest/src/main/java/com/palazzisoft/skyquest/SkyQuestApplication.java
@@ -2,8 +2,10 @@ package com.palazzisoft.skyquest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@EnableDiscoveryClient
+@SpringBootApplication
 public class SkyQuestApplication {
 
 	public static void main(String[] args) {
--- config-server/src/main/java/com/palazzisoft/ConfigServerApp.java
+++ config-server/src/main/java/com/palazzisoft/ConfigServerApp.java
@@ -2,8 +2,10 @@ package com.palazzisoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApp {
 
