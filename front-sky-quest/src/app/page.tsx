c57445b0c2628a28a37c54adcba601237fe8ad05
ppalazzi <ppalazzi@gmail.com>
2025-06-18 16:21:56 -0300

    SQ-25: [FE] Sidebar component (#18)

--- front-sky-quest/src/app/page.tsx
+++ front-sky-quest/src/app/page.tsx
@@ -4,13 +4,15 @@ import Sidebar from '@/components/sidebar/Sidebar';
 export default function Home() {
 	return (
 		<div
-			className="flex flex-col justify-items-center w-full min-h-screen">
+			className="flex flex-col justify-items-center w-full h-screen">
 			<header className="sticky top-0 w-full">
 				<TopBar/>
 			</header>
 
-			<main className="flex-1 border-1 w-full h-full">
-				<Sidebar />
+			<main className="flex flex-1 border-1 w-full h-full">
+				<aside className="w-60 h-full border-r border-gray-200">
+					<Sidebar />
+				</aside>
 			</main>
 
 			<footer className="flex justify-center w-full mt-auto">
